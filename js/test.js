73f330d92adc0dc81d4e155baf7f1d33818c9f93
ksarjib <ksarjib@gmail.com>
2021-07-30 03:28:13 -0500

    fix

--- js/test.js
+++ js/test.js
@@ -2,15 +2,15 @@ describe("Prototype", () => {
   describe("Filter String", () => {
     it("should return 'It is a nice day!'", () => {
       assert.equal(
-        "It is a nice day!",
-        "It is a very nice day!".filter(['very'])
+        "It is a very nice day!".filter(['very']),
+        "It is a nice day!"
         
       );
     });
   });
   describe('Bubble Sort', () => {
     it('should return [-2, 0, 1, 3, 4, 6]', () => {
-      assert.deepEqual([-2, 0, 1, 3, 4, 6], [6, 4, 0, 3, -2, 1].bubbleSort());
+      assert.deepEqual([6, 4, 0, 3, -2, 1].bubbleSort(), [-2, 0, 1, 3, 4, 6]);
     });
   });
 });
