a8431cf3365dc8f2bd6cbe0597d0607b101d0632
ksarjib <ksarjib@gmail.com>
2021-07-30 03:20:10 -0500

    validation fix

--- js/test.js
+++ js/test.js
@@ -2,15 +2,15 @@ describe("Prototype", () => {
   describe("Filter String", () => {
     it("should return 'This is a sentence'", () => {
       assert.equal(
-        "This is a nice sentence",
         "This is not a nice sentence".filter(['not']),
+        "This is a nice sentence"
         
       );
     });
   });
   describe('Bubble Sort', () => {
     it('should return [-2, 0, 1, 3, 4, 6]', () => {
-      assert.deepEqual([-2, 0, 1, 3, 4, 6], [6, 4, 0, 3, -2, 1].bubbleSort());
+      assert.deepEqual([6, 4, 0, 3, -2, 1].bubbleSort(), [-2, 0, 1, 3, 4, 6]);
     });
   });
 });
