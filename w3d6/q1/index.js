fdc93dfa982931c22876e95944553d5506d3712f
ksarjib <ksarjib@gmail.com>
2021-08-08 18:05:47 -0500

    fix

--- w3d6/q1/index.js
+++ w3d6/q1/index.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, "view"));
-app.use('/css', express.static(path.join(__dirname, '../', 'css')));
+app.use('/css', express.static(path.join(__dirname, 'css')));
 app.get('/', (req, res) => {
     const date = new Date();
     const hour = date.getHours();
